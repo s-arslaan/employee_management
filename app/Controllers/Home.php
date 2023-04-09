a065634c01cb4a263f68503a7252bd1c477999b2
Arslaan Shaikh <shaikharslaan1234@gmail.com>
2023-04-09 14:06:21 +0530

    home page added

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'title' => APP_NAME.' | Home',
+        ];
+
+        return view('index_view', $data);
     }
 }
